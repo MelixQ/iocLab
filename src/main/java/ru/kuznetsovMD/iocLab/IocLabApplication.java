b156148fcf6ae1a7878ada9f7f6d16a68d0af18a
Maxim Kuznetsov <melixmilan@gmail.com>
2025-10-22 18:47:49 +0500

    Initial commit

---
+++ src/main/java/ru/kuznetsovMD/iocLab/IocLabApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsovMD.iocLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IocLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kuznetsovMD/iocLab/IocLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsovMD.iocLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
